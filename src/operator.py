bf09401e3537240e1cfc1b4bc192193160079211
hyvercode <hyvercode@gmail.com>
2024-12-23 22:00:14 +0700

    Operator aritmatika

---
+++ src/operator.py
@@ -0,0 +1,16 @@
+#Operator Aritmatika
+
+#Penjumlahan (+)
+print(10+5)  #15
+#Pengurangan  (-)
+print(10-5) #5
+#Perkalian (*)
+print(5*5) #25
+#Pembagian (/)
+print(100/10) #10
+#Modulus (%)
+print(100 % 5) #10
+#Perpangkatan (**)
+print(5**5) #3125
+#Pembagian Bulan (//)
+print(31256 //13)  #2404
