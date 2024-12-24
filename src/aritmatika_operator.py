2ac854375816986f0b91ad7c3dd028028eee96e4
mohirwanh <mohirwanh@gmail.com>
2024-12-24 14:03:02 +0700

    Aritmatika

---
+++ src/aritmatika_operator.py
@@ -0,0 +1,31 @@
+# Operator Aritmatika
+
+def penjumlahan(a, b):
+    return a + b
+
+def pengurangan(a, b):
+    return a - b
+
+def perkalian(a, b):
+    return a * b
+
+def pembagian(a, b):
+    return a / b
+
+def modulus(a, b):
+    return a % b
+
+def perpangkatan(a, b):
+    return a ** b
+
+def pembagian_bulan(a, b):
+    return a // b
+
+# Example usage
+print(penjumlahan(10, 5))  # 15
+print(pengurangan(10, 5))  # 5
+print(perkalian(5, 5))  # 25
+print(pembagian(100, 10))  # 10
+print(modulus(100, 5))  # 0
+print(perpangkatan(5, 5))  # 3125
+print(pembagian_bulan(31256, 13))  # 2404
