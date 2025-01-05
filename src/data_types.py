aa7a209bad9ba18ef8ae8c8e95d2e025335d4f65
Hyvercode <hyvercode@gmail.com>
2025-01-05 22:37:58 +0700

    data types

---
+++ src/data_types.py
@@ -0,0 +1,11 @@
+name="Jhon"
+
+name = "Jhon"
+type(name) == str #True
+print(type(name) == str )
+
+
+name = "Jhon"
+result = isinstance(name, str) #True
+
+print("varibale name is string ? ",result)
