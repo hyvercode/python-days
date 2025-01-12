1ecb4232483a85a5b02a97adb5303c7ee02f7212
Hyvercode <hyvercode@gmail.com>
2025-01-12 10:40:49 +0700

    udpated

---
+++ src/boolean.py
@@ -0,0 +1,17 @@
+# Operator and
+print(True and True)   # Output: True
+print(True and False)  # Output: False
+
+# Operator or
+print(True or False)   # Output: True
+print(False or False)  # Output: False
+
+# Operator not
+print(not True)        # Output: False
+print(not False)       # Output: True
+
+if not "":
+    print("String kosong dianggap False")
+
+if not []:
+    print("List kosong dianggap False")
