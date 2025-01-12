eaa3cf9ac6dc6e109c59c7eece1c9e3e94ef18e5
Hyvercode <hyvercode@gmail.com>
2025-01-12 10:03:46 +0700

    Update String

--- src/string.py
+++ src/string.py
@@ -14,3 +14,30 @@ name += "Age 23"
 print(name) #Jhon Age 23
 
 print(name)
+
+#Convert number to string
+print("Jhon is " + str(8) + " years old") #Jhon is 8 years old
+
+
+print("""Jhon is
+    8
+years old
+""")
+#double quotes, or single quotes
+print('''
+Jhon is
+8
+years old
+''')
+
+#lower case
+name = "Jhon"
+print(name.lower()) #"jhon"
+
+#len
+name = "Jhon"
+print(len(name)) #5
+
+#in
+name = "Jhon"
+print("hon" in name) #True
