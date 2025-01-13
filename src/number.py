ffe08674a54d3bc982e19351b9569b444ea5c187
Hyvercode <hyvercode@gmail.com>
2025-01-13 20:51:59 +0700

    Number

---
+++ src/number.py
@@ -0,0 +1,18 @@
+#int
+age = 8
+print(type(age)) #true
+
+price = 1000
+qty=5
+
+total = price * qty
+
+print("Tota is "+str(total)) #5000
+
+#floating
+price = 1000
+weight=4.5
+
+_total = price * weight
+
+print("Tota is "+str(_total)) #5000
