c4a55c3e60e7a807b9cd6e110b6b5049a8f3a092
Hyvercode <hyvercode@gmail.com>
2025-10-22 06:40:30 +0700

    constan  day

---
+++ src/constant.py
@@ -0,0 +1,12 @@
+from typing import Final
+
+#explisit data tipe
+PI:Final=3.14
+
+print("pi :%f" % (PI))
+
+#implisit data type
+
+TOTAL_MONTH:Final[int]=12
+
+print("Total Month %i" % (TOTAL_MONTH))
